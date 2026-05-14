642b2a06773faf79e43e6de863bb9697dc8bdf89
Smyrfy <dudko.dasha72773@gmail.com>
2026-05-14 18:18:12 +0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "n^2 + 3n + 1 = Theta(n^2)" << endl;
+
+    cout << "Для O(n^2): C = 5, n0 = 1" << endl;
+    cout << "Для Omega(n^2): c = 1, n0 = 1" << endl;
+
+    return 0;
+}
