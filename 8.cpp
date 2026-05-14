1927541379b3f12aee2c2240f4250c31a4d06b17
Smyrfy <dudko.dasha72773@gmail.com>
2026-05-14 18:17:44 +0300

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int C = 12;
+    int n0 = 1;
+
+    cout << "5n + 7 = O(n)" << endl;
+    cout << "C = " << C << endl;
+    cout << "n0 = " << n0 << endl;
+
+    return 0;
+}
